f1a271d1101a9717cc9a77ad77a3cd956b3a7377
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-08-07 03:06:25 +0530

    Update stream.py

--- AbhiXmusic/utils/stream/stream.py
+++ AbhiXmusic/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from AnonXMusic import Carbon, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import AnonyBin
-from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
-from AnonXMusic.utils.thumbnails import get_thumb
+from AbhiXmusic import Carbon, YouTube, app
+from AbhiXmusic.core.call import Abhi
+from AbhiXmusic.misc import db
+from AbhiXmusic.utils.database import add_active_video_chat, is_active_chat
+from AbhiXmusic.utils.exceptions import AssistantErra
+from AbhiXmusic.utils.inline import aq_markup, close_markup, stream_markup
+from AbhiXmusic.utils.pastebin import AnonyBin
+from AbhiXmusic.utils.stream.queue import put_queue, put_queue_index
+from AbhiXmusic.utils.thumbnails import get_thumb
 
 
 async def stream(
