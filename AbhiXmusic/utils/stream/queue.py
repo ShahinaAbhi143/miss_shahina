457ffe31c9600767cd08b9b4b58e4b0dd0bebcc1
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-08-07 03:04:02 +0530

    Update queue.py

--- AbhiXmusic/utils/stream/queue.py
+++ AbhiXmusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.formatters import check_duration, seconds_to_min
+from AbhiXmusic.misc import db
+from AbhiXmusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
