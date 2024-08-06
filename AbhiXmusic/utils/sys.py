606b22ec99c85925ef0de0184eefdbbb3cd8c080
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-08-07 02:34:23 +0530

    Update sys.py

--- AbhiXmusic/utils/sys.py
+++ AbhiXmusic/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from AnonXMusic.misc import _boot_
-from AnonXMusic.utils.formatters import get_readable_time
+from AbhiXmusic.misc import _boot_
+from AbhiXmusic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
