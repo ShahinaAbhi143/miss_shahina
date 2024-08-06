8f2753cb462c167bcb1965644602a49b71d0180d
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-08-07 02:14:30 +0530

    Update live.py

--- AbhiXmusic/plugins/play/live.py
+++ AbhiXmusic/plugins/play/live.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.stream.stream import stream
+from AbhiXmusic import YouTube, app
+from AbhiXmusic.utils.channelplay import get_channeplayCB
+from AbhiXmusic.utils.decorators.language import languageCB
+from AbhiXmusic.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
