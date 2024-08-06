fa08c04a87b14f62d0f1179d19ddd88bd64050c4
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-08-07 02:30:55 +0530

    Update channelplay.py

--- AbhiXmusic/utils/channelplay.py
+++ AbhiXmusic/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from AbhiXmusic import app
+from AbhiXmusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
